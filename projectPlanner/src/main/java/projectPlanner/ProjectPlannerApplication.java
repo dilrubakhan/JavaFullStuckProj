f2252bb869000364afbe9a582fdc762bd90de9fa
Dilruba Khan <rubatanisa@yahoo.ie>
2020-04-27 22:28:32 +0100

    first commit

---
+++ projectPlanner/src/main/java/projectPlanner/ProjectPlannerApplication.java
@@ -0,0 +1,15 @@
+package projectPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPlannerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProjectPlannerApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
---
+++ projectPlanner/src/test/java/projectPlanner/ProjectPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package projectPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
