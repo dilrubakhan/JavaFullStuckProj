b7422441224fa42f4262a6a7d16356d5e4ec11d8
Dilruba Khan <rubatanisa@yahoo.ie>
2020-04-30 19:10:21 +0100

    Custom exception for project identifier

--- projectPlanner/src/main/java/projectPlanner/services/ProjectService.java
+++ projectPlanner/src/main/java/projectPlanner/services/ProjectService.java
@@ -3,6 +3,7 @@ package projectPlanner.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import projectPlanner.domin.Project;
+import projectPlanner.exceptions.ProjectIdException;
 import projectPlanner.repositories.ProjectRepository;
 
 @Service
@@ -12,10 +13,11 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
-
-        //Logic
-
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
     }
-
 }
