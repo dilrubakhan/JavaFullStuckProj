d0a55b1ff745b49487c328f309120ec9f589760b
Dilruba Khan <rubatanisa@yahoo.ie>
2020-05-02 19:54:22 +0100

    Find project by project identifier

--- projectPlanner/src/main/java/projectPlanner/repositories/ProjectRepository.java
+++ projectPlanner/src/main/java/projectPlanner/repositories/ProjectRepository.java
@@ -7,6 +7,5 @@ import projectPlanner.domin.Project;
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
 
-    @Override
-    Iterable<Project> findAllById(Iterable<Long> iterable);
+    Project findByProjectIdentifier(String projectId);
 }
--- projectPlanner/src/main/java/projectPlanner/web/ProjectController.java
+++ projectPlanner/src/main/java/projectPlanner/web/ProjectController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import projectPlanner.domin.Project;
 import projectPlanner.services.MapValidationErrorService;
 import projectPlanner.services.ProjectService;
@@ -25,13 +22,20 @@ public class ProjectController {
     private MapValidationErrorService mapValidationErrorService;
 
     @PostMapping("")
-    public ResponseEntity< ? > createNewProject(@Valid @RequestBody Project project, BindingResult result){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 
-        ResponseEntity errorMap = mapValidationErrorService.MapValidationService(result);
-        if(errorMap!= null) return errorMap;
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap!=null) return errorMap;
 
         Project project1 = projectService.saveOrUpdateProject(project);
-        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+        return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
     }
 
+    @GetMapping("/{projectId}")
+    public ResponseEntity<?> getProjectById(@PathVariable String projectId){
+
+        Project project = projectService.findProjectByIdentifier(projectId);
+
+        return new ResponseEntity<Project>(project, HttpStatus.OK);
+    }
 }
